3ab8918c432b082271105837d081bdbed1b2debc
Mohamedsaad994 <ilhsm672016@gmail.com>
2025-06-28 16:29:18 +0300

    create layout & header component

--- Client/gulpfile.js
+++ Client/gulpfile.js
@@ -3,5 +3,5 @@ const gulpSass = require("gulp-sass");
 const sass = gulpSass(require("sass"));
 
 gulp.task("sass", async function () {
-  gulp.src("src/*.scss").pipe(sass()).pipe(gulp.dest("src/Css"));
+  gulp.src("src/Components/**/*.scss").pipe(sass()).pipe(gulp.dest("src/Css"));
 });
