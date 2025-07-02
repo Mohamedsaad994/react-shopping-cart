592520749b272152142f61f1936932ff8c192a57
Mohamedsaad994 <ilhsm672016@gmail.com>
2025-07-02 10:03:39 +0300

    handle products design

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,12 +1,21 @@
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
+import data from './data.json'
+import { useState } from "react";
+import Products from "./Components/Products/Products";
 
 function App() {
+  const [products, setProducts] = useState(data)
   return (
     <div className="layout">
-      <Header/>
-      <main>Content</main>
-      <Footer/>
+      <Header />
+      <main>
+        <div className="wrapper">
+          <Products products={products}/>
+          <div className="filters">Filters</div>
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 }
