786305a0c12754b9f0b47620ef63cb7896e213e9
Mohamedsaad994 <ilhsm672016@gmail.com>
2025-07-03 22:26:44 +0300

    handle product details

--- Client/src/Components/Products/Products.js
+++ Client/src/Components/Products/Products.js
@@ -1,18 +1,31 @@
-import '../../Css/Products/Products.css'
+import { useState } from "react";
+import "../../Css/Products/Products.css";
+import ProductModal from "./ProductModal";
 
 function Products(props) {
+  const [product, setProduct] = useState();
+  const openModal = (product) => {
+    setProduct(product);
+  };
+  const closeModal = () => {
+    setProduct(false);
+  };
   return (
     <div className="products">
       {props.products.map((product) => (
         <div className="product-item" key={product.id}>
-          <img src={product.imageUrl} alt={product.title} />
+          <a href="#" onClick={() => openModal(product)}>
+            <img src={product.imageUrl} alt={product.title} />
+          </a>
           <div className="product-desc">
             <p>{product.title}</p>
-            <span>{product.price}</span>
+            <span>$ {product.price}</span>
           </div>
           <button>Add To Cart</button>
         </div>
       ))}
+
+      <ProductModal product={product} closeModal={closeModal}/>
     </div>
   );
 }
