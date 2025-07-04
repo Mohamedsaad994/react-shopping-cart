d0001e2daafb9a80bdf6d962f3b410f5db4b3498
Mohamedsaad994 <ilhsm672016@gmail.com>
2025-07-04 23:58:37 +0300

    handle filter logic

--- Client/src/Components/Filters/Filters.js
+++ Client/src/Components/Filters/Filters.js
@@ -1,28 +1,32 @@
-import '../../Css/Filters/Filters.css'
+import "../../Css/Filters/Filters.css";
 
-function Filters() {
+function Filters(props) {
   return (
     <div className="filters">
       <h2 className="filters-title">Filters</h2>
       <div className="num-of-products">Number of products 4</div>
       <div className="filter-by-size">
         <span>Filter</span>
-        <select className="filter-select">
-            <option value="ALL">ALL</option>
-            <option value="XS">XS</option>
-            <option value="S">S</option>
-            <option value="M">M</option>
-            <option value="L">L</option>
-            <option value="XL">XL</option>
-            <option value="XXL">XXL</option>
+        <select
+          className="filter-select"
+          value={props.size}
+          onChange={props.handleFilterSize}
+        >
+          <option value="ALL">ALL</option>
+          <option value="XS">XS</option>
+          <option value="S">S</option>
+          <option value="M">M</option>
+          <option value="L">L</option>
+          <option value="XL">XL</option>
+          <option value="XXL">XXL</option>
         </select>
       </div>
       <div className="filter-by-size">
         <span>Order</span>
-        <select className="filter-select">
-            <option value="lowest">Lowest</option>
-            <option value="lowter">Lowter</option>
-            <option value="latest">Latest</option>
+        <select className="filter-select" value={props.sort} onChange={props.handleFilterSort}>
+          <option value="lowest">Lowest</option>
+          <option value="highest">Highest</option>
+          <option value="latest">Latest</option>
         </select>
       </div>
     </div>
