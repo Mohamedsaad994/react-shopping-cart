5897d89a6babb11a9691f3e5b14c131711b53979
Mohamedsaad994 <ilhsm672016@gmail.com>
2025-07-05 17:07:37 +0300

    handle cart logic

--- Client/src/Components/Cart/Cart.js
+++ Client/src/Components/Cart/Cart.js
@@ -4,7 +4,15 @@ function Cart(props) {
   return (
     <div className="cart">
       <div className="cart-header">
-        <h3>Empty Cart</h3>
+        <h3>
+          {props.cartItems.length === 0
+            ? "Empty Cart"
+            : `There ${props.cartItems.length === 1 ? "is" : "are"} ${
+                props.cartItems.length
+              } ${
+                props.cartItems.length === 1 ? "product" : "products"
+              } in your cart`}
+        </h3>
       </div>
       <div className="cart-items">
         {props.cartItems.map((item) => (
@@ -12,11 +20,11 @@ function Cart(props) {
             <img src={item.imageUrl} alt={item.title} />
             <div className="cart-info">
               <div>
-                <p>{item.title}</p>
-                <p>Qty</p>
-                <p>$ {item.price}</p>
+                <p>Product Name: {item.title}</p>
+                <p>Qty: {item.qty}</p>
+                <p>Product Price: $ {item.price}</p>
               </div>
-              <button>Remove</button>
+              <button onClick={() => props.removeFromCart(item)}>Remove</button>
             </div>
           </div>
         ))}
